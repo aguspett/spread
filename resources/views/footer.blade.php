73df7a67b07b55f5c49c98ca65ba0ebf5a79fedb
agustin <agus.sys.spread@gmail.com>
2015-09-03 18:32:09 -0300

    mejoras en layout y routing add adminlte

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Spread Electric s.a
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2015 <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
